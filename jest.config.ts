d13e7a6be11249df9b25e8b04f83c576372e9671
bantheus <matheuschmidt.b@gmail.com>
2024-03-06 15:02:57 -0300

    ✨ Feat: add jest setup for tests

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageReporters: ["json"],
+  setupFilesAfterEnv: ["./tests/jest.setup.ts"],
+  testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  transform: { "^.+\\.(ts|tsx)$": "ts-jest" },
+};
+
+export default config;
