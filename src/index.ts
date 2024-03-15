f251c4fa631f1b59b7620c9e49a33e1309a139e4
bantheus <matheuschmidt.b@gmail.com>
2024-03-15 14:58:28 -0300

    ♻️ Refactor: changed start server and prisma migrate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
+import { execSync } from "child_process";
 import { server } from "./server/server";
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`🚀 Server is running in http://localhost:${process.env.PORT || 3333}`);
-});
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`🚀 Server is running in http://localhost:${process.env.PORT || 3333}`);
+  });
+};
+
+if (process.env.IS_LOCALHOST !== "true") {
+  try {
+    execSync("npx prisma migrate deploy");
+    console.log("Prisma migrations executed successfully!");
+    startServer();
+  } catch (error) {
+    console.error("Error executing Prisma migrations:", error);
+    process.exit(1);
+  }
+} else {
+  startServer();
+}
+
+
